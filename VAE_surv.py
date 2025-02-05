bda299c327f9217fd3a44f1a5105bc20d68c15b3
Cesare Rollo <cesare.rollo@unito.it>
2025-02-05 16:59:16 +0000

    Update VAE-Surv class

--- VAE_surv.py
+++ VAE_surv.py
@@ -1,9 +1,18 @@
+import numpy
+import pandas
+from typing import List
+#import matplotlib.pyplot as plt
 import torch
-import numpy as np
-from torch import Tensor
-import torch.nn as nn
 import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset
+from loss import cox_ph_loss, cox_ph_loss_sorted
+from tqdm import tqdm
+from sksurv.util import check_array_survival
+from sksurv.metrics import concordance_index_censored, concordance_index_ipcw
+import dataclasses
 
+import sklearn
 
 class NegativeBinomialLoss(nn.Module):
     def __init__(self, eps=1e-10):
@@ -16,194 +25,325 @@ class NegativeBinomialLoss(nn.Module):
         return torch.mean(-n*torch.log(p + self.eps) - (1 - n)*torch.log(1 - p + self.eps))
 
 class LogCoshLoss(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
     def forward(self, y_t, y_prime_t):
         ey_t = y_t - y_prime_t
         return torch.mean(torch.log(torch.cosh(ey_t + 1e-12)))
 
-    
-    
-    
+
+_losses = {
+    'MSE': nn.MSELoss,
+    'MAE': nn.L1Loss,
+    'NBL': NegativeBinomialLoss,
+    'LCH': LogCoshLoss,
+}
+
+def get_activation(activation_name: str):
+    try:
+        return getattr(nn, activation_name)()
+    except AttributeError:
+        raise ValueError(f"Invalid activation function '{activation_name}'")
+
+def get_loss(loss_name: str):
+    try:
+        return _losses[loss_name]()
+    except KeyError:
+        raise ValueError(f"Invalid criterion '{loss_name}', must be one of {','.join(_losses)}")
+
+def make_fnn_layers(dims, activation, dropout = 0.0, batchnorm = False):
+    activation_mod = get_activation(activation)
+    dropout_mod = nn.Dropout(dropout)
+    layers = []
+    for i, input_size in enumerate(dims[:-1]):
+        is_hidden_layer = i < len(dims) - 2
+        output_size = dims[i + 1]
+        layers.append(nn.Linear(input_size, output_size))
+        if is_hidden_layer:
+            if batchnorm:
+                layers.append(nn.BatchNorm1d(output_size))
+            if dropout > 0.0:
+                layers.append(dropout_mod)
+            layers.append(activation_mod) # FIXME va prima activation o dropout?
+    return nn.Sequential(*layers)
+
+
 class VAE(nn.Module):
-    def __init__(self, encoding_nodes, latent_dim, surv_nodes, activation, dropout_vae, dropout_coxnet, use_batchnorm,
-                 pretrain, KL_weight, loss_criterion):
-        super(VAE, self).__init__()
-        
-        self.encoder_dims = encoding_nodes
-        self.surv_dims = surv_nodes
-        self.pretrain = pretrain
-        self.K = KL_weight
-        self.use_batchnorm = use_batchnorm
+    def __init__(self, hidden_dims, latent_dim, **kwargs):
+        super().__init__()
         self.latent_dim = latent_dim
-        
-        self.num_genetic = 58 
 
-        
-        if self.pretrain == True:
-            print('Pretraining Autoencoder: True')
-        
-        self.encoding_layers = nn.ModuleList()
-        self.decoding_layers = nn.ModuleList()
-        self.surv_layers = nn.ModuleList()
-        
-        
-        # Add activation function
-        if activation == 'relu':
-            self.activation = nn.ReLU()
-        elif activation == 'sigmoid':
-            self.activation = nn.Sigmoid()
-        elif activation == 'tanh':
-            self.activation = nn.Tanh()
-        elif activation == 'softplus':
-            self.activation = nn.Softplus()
-        elif activation == 'leaky relu':
-            self.activation = nn.LeakyReLU()
-        elif activation == 'elu':
-            self.activation = nn.ELU()
-        else:
-            raise ValueError("Invalid activation function")
-            
-        
-        if loss_criterion == 'mse':
-            self.criterion = nn.MSELoss()
-        if loss_criterion == 'mae':
-            self.criterion = nn.L1Loss()
-        if loss_criterion == 'nbl':
-            self.criterion = NegativeBinomialLoss()
-        if loss_criterion == 'logcosh':
-            self.criterion = LogCoshLoss()
-        
-        # Add dropout
-        self.dropout_vae = nn.Dropout(dropout_vae)
-        self.dropout_coxnet = nn.Dropout(dropout_coxnet)
-        
-        # Add encoding layers
-        num_layers = len(self.encoder_dims)
-        
-        for i in range(num_layers-1):
-            self.encoding_layers.append(nn.Linear(self.encoder_dims[i], self.encoder_dims[i+1]))
-            
-            #if self.use_batchnorm:
-            #    self.encoding_layers.append(nn.BatchNorm1d(self.encoder_dims[i+1]))
-            self.encoding_layers.append(self.activation)
-            self.encoding_layers.append(self.dropout_vae)
-            
-        self.fc_mu = nn.Linear(self.encoder_dims[-1], latent_dim)
-        self.fc_logvar = nn.Linear(self.encoder_dims[-1], latent_dim)
-        
-        
-        
-        
-        # Add decoding layers
-        self.decoding_layers.append(nn.Linear(latent_dim, self.encoder_dims[-1]))
-        self.decoding_layers.append(self.dropout_vae)
-        
-        for i in range(num_layers-1, 0, -1):
-            self.decoding_layers.append(nn.Linear(self.encoder_dims[i], self.encoder_dims[i-1]))
-            #if self.use_batchnorm:
-            #        self.decoding_layers.append(nn.BatchNorm1d(self.encoder_dims[i-1]))
-            self.decoding_layers.append(self.activation)
-            
-            
-            if i != 1:
-                self.decoding_layers.append(self.dropout_vae)
-            
-            
-            
-        if self.surv_dims is not None:
-            # Add coxnet layers
-            num_surv_layers = len(self.surv_dims) - 1
-            for i in range(num_surv_layers):
-                self.surv_layers.append(nn.Linear(self.surv_dims[i], self.surv_dims[i+1]))
-                #if i==0:
-                #    self.surv_layers.append(nn.BatchNorm1d(self.surv_dims[i+1]))
+        encoder_dims = hidden_dims + [latent_dim * 2]
+        self.encoder = make_fnn_layers(encoder_dims, **kwargs)
+        decoder_dims = [latent_dim] + hidden_dims[::-1]
+        self.decoder = make_fnn_layers(decoder_dims, **kwargs)
 
-
-    
     def encode(self, x):
-        for layer in self.encoding_layers:
-            x = layer(x)
-        mu = self.fc_mu(x)
-        logvar = self.fc_logvar(x)
-        return mu, logvar
+        z2 = self.encoder(x)
+        mean = z2[..., :self.latent_dim]
+        logvar = z2[..., self.latent_dim:]
+        return mean, logvar
 
-    # In the decode function
     def decode(self, z):
-        for layer in self.decoding_layers:
-            z = layer(z)
-        return z
+        return self.decoder(z)
+    
+    def forward(self, x):
+        mean, logvar = self.encode(x)
+        softclamped_logvar = torch.clamp(logvar, min=-10.0, max=10.0)  #alternatively: torch.tanh(logvar / 10.0) * 10.0 
+        stddev = torch.exp(0.5 * softclamped_logvar)
+        z = mean + stddev*torch.randn_like(stddev) if self.training else mean # FIXME do we want this?
+        r = self.decode(z)
+        return dict(
+            mean = mean,
+            logvar = softclamped_logvar,
+            stddev = stddev,
+            zrep = z, # reparametrized z
+            rec = r,
+        )
+    
+class CoxNet(nn.Module):
+    def __init__(self, hidden_dims, **kwargs):
+        super().__init__()
+        self.layers = make_fnn_layers(hidden_dims + [1], **kwargs)
+
+    def forward(self, x):
+        risk = self.layers(x) # FIXME originariamente ha tanh activation
+        return risk
+
+class TrainHistory:
+    def __init__(self):
+        self.history = []
+        self.last_epoch = None
+    
+    def add_batch_losses(self, epoch, dataset, losses):
+        # increasing epoch 
+        if self.last_epoch is not None:
+            assert epoch == self.last_epoch or epoch == self.last_epoch + 1, 'epoch must increase'
+        self.last_epoch = epoch
+
+        for loss_name, loss_value in losses.items():
+            if hasattr(loss_value, 'item'):
+                loss_value = loss_value.item()
+            self.history.append([epoch, dataset, loss_name, loss_value])
+
+    def get_df(self):
+        return pandas.DataFrame.from_records(self.history, columns=['epoch', 'dataset', 'loss_name', 'loss_value'])
     
 
-    
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)
-        z = mu + eps*std
-        return z
-    
-    
-    
-    def coxnet(self, x_concat):
-        for i, layer in enumerate(self.surv_layers):
-            x_concat = layer(x_concat)
-            if i != len(self.surv_layers) - 1:
-                x_concat = self.dropout_coxnet(x_concat) #self.dropout
+@dataclasses.dataclass
+class VAESurv(sklearn.base.BaseEstimator):
+    # VAESurv
+    vae_hidden_dims: List[int] = dataclasses.field(default_factory=list)
+    vae_latent_dim: int = 2
+    vae_activation: str = 'Tanh'
+    vae_dropout: float = 0.0
+    vae_batchnorm: bool = False
+    vae_loss: str = 'LCH'
 
-            # apply tanh activation if this is the last layer, else use the predefined activation
-            if i == len(self.surv_layers) - 1:
-                x_concat = nn.Tanh()(x_concat)
-            else:
-                x_concat = nn.Tanh()(x_concat) #nn.Tanh()(x_concat) #self.activation(x_concat)
+    # survival
+    survival_hidden_dims: List[int] = dataclasses.field(default_factory=list)
+    survival_activation: str = 'Tanh'
+    survival_dropout: float = 0.0
+    survival_batchnorm: bool = False
 
-        x_concat = torch.exp(x_concat)
+    # other
+    vae_feature_mask: numpy.ndarray = None
+    alpha: float = 0.5
+    kl_weight: float = 0.001
 
-        return x_concat
+    # training
+    vae_epochs: int = 10
+    combo_epochs: int = 10
+    survival_epochs: int = 0
+    vae_learning_rate: float = 1e-4
+    combo_learning_rate: float = 1e-4
+    survival_learning_rate: float = 1e-4
+    batch_size: int = 64
 
-    
-    
-    def forward(self, x, c):
-        mu, logvar = self.encode(x)
-        logvar = torch.clamp(logvar, max=10)
+    vae_weight_decay: float = 0.
+    survival_weight_decay: float = 0.
+    combo_weight_decay: float = 0.
+
+    # other
+    device: str = 'cpu'
+    verbose: int = 2
+
+    def fit(self, X, y, X_test=None, y_test=None):
+        # FIXME handle pandas dataframes
+
+        # dataset prep
+        #if self.vae_feature_mask is None:
+            # use all features for the VAE
+        #    self.vae_feature_mask = numpy.full(X.shape[1], True)
             
-        z = self.reparameterize(mu, logvar)
-        
-        # Standardize z
-        #mu_z = torch.mean(z, dim=0, keepdim=True)
-        #std_z = torch.std(z, dim=0, keepdim=True)
-        #z_standardized = (z - mu_z) / (std_z + 1e-9)
-        
-        if self.pretrain is False and c is not None:
-            x_concat = torch.hstack([z,c]) 
-            risk = self.coxnet(x_concat)
-            
+        if self.vae_feature_mask is None:
+            self.vae_feature_mask = numpy.full(X.shape[1], True, dtype=bool)
         else:
-            risk=None
-            
-        rec = self.decode(z)
+            self.vae_feature_mask = numpy.array(self.vae_feature_mask, dtype=bool)
+    
+        vae_n_feats = self.vae_feature_mask.sum()
+        surv_n_feats = (~self.vae_feature_mask).sum() + self.vae_latent_dim
+
+        train_dataset = self._get_tensor_dataset(X, y)
+        train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+        if X_test is not None:
+            test_dataset = self._get_tensor_dataset(X_test, y_test)
+            test_loader = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False)
+        else:
+            test_loader = None
+
+        # model init
+        self.modules_ = nn.ModuleDict({
+            'VAE': VAE(
+            [vae_n_feats] + self.vae_hidden_dims, self.vae_latent_dim, 
+            activation=self.vae_activation,
+            dropout=self.vae_dropout,
+            batchnorm=self.vae_batchnorm,
+        ),
+            'survival': CoxNet(
+            [surv_n_feats] + self.survival_hidden_dims,
+            activation=self.survival_activation,
+            dropout=self.survival_dropout,
+            batchnorm=self.survival_batchnorm,
+        ),
+            'reconstruction_loss': get_loss(self.vae_loss),
+        }).to(self.device)
+
+        # model training
+        self.train_history_ = TrainHistory()
+        progress = tqdm if self.verbose >= 2 else lambda x, desc: x
+
+        # VAE pretraining    
+        optimizer = optim.Adam(
+            self.modules_['VAE'].parameters(), 
+            lr=self.vae_learning_rate,
+            weight_decay=self.vae_weight_decay)
+        for epoch in progress(range(self.vae_epochs), desc='VAE pretraining'):
+            self._train(epoch, optimizer, train_loader, test_loader, train_survival=False)
+
+        # combo training
+        optimizer = optim.Adam(
+            self.modules_.parameters(),
+            lr=self.combo_learning_rate,
+            weight_decay=self.combo_weight_decay)
+        for epoch in progress(range(self.vae_epochs, self.vae_epochs + self.combo_epochs), desc='Training'):
+            self._train(epoch, optimizer, train_loader, test_loader, train_survival=True)
+            if test_loader is not None:
+                risk_test = self.predict(X_test)
+                self.train_history_.add_batch_losses(epoch, 'test', {
+                    'c-index': concordance_index_censored(
+                        event_indicator=y_test['event'],
+                        event_time=y_test['time'],
+                        estimate=risk_test,
+                    )[0],
+            })
         
-        return rec, z, logvar, risk 
-    
-    
-    def predict_risk(self, data):
-        gen = torch.tensor(data[:,:self.num_genetic], dtype = torch.float32)
-        clin = torch.tensor(data[:,self.num_genetic:], dtype = torch.float32)
-        return self.forward(gen, clin)[-1]  ###### 1 ######
-    
-    
-    
-    def loss_function(self, rec_gen, rec_cyto, x_gen, x_cyto, mu, logvar):
-        # Compute reconstruction loss
-        reconstruction_loss = self.criterion(rec_gen,x_gen) + self.criterion(rec_cyto,x_cyto)
+        # survival training
+        optimizer = optim.Adam(
+            self.modules_['survival'].parameters(),
+            lr=self.survival_learning_rate,
+            weight_decay=self.survival_weight_decay)
+        for epoch in progress(range(self.vae_epochs + self.combo_epochs, self.vae_epochs + self.combo_epochs + self.survival_epochs), desc='Post training'):
+            self._train(epoch, optimizer, train_loader, test_loader, train_survival=True)
+            # FIXME test_loader has shuffle=True, cannot use for c-index with the original y_test...
+            if test_loader is not None:
+                risk_test = self.predict(X_test)
+                self.train_history_.add_batch_losses(epoch, 'test', {
+                    'c-index': concordance_index_censored(
+                        event_indicator=y_test['event'],
+                        event_time=y_test['time'],
+                        estimate=risk_test,
+                    )[0],
+            })
+
+
+    def predict(self, X, return_all=False):
+        results = self._run_model(X, run_survival=True)
+        return results if return_all else results['risk'][:, 0]
         
-        # Compute KL divergence loss
-        kl_divergence = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-        kl_divergence = self.K*kl_divergence
-            
-       
-        # Combine losses
-        loss = reconstruction_loss + kl_divergence
+    def transform(self, X, return_all=False):
+        results = self._run_model(X, run_survival=False)
+        return results if return_all else results['mean']
+
+    def score(self, X, y):
+        event, time = check_array_survival(X, y)
+        return concordance_index_censored(event_indicator=event, event_time=time, estimate=self.predict(X))[0]
+
+    def _get_tensor_dataset(self, X, y):
+        # Ensure X is correctly formatted
+        if isinstance(X, torch.Tensor):  
+            X = X.clone().detach().cpu().numpy()  # Convert Tensor to NumPy to avoid slicing issues
+        elif hasattr(X, 'values'):  # Pandas DataFrame
+            X = X.to_numpy()  # Explicit conversion to NumPy
         
-        return loss, reconstruction_loss, kl_divergence  
+
+        Xvae = X[:, self.vae_feature_mask]
+        Xsurvival = X[:, ~self.vae_feature_mask]
+
+        ts = (
+            torch.tensor(Xvae, dtype=torch.float32, device=self.device),
+            torch.tensor(Xsurvival, dtype=torch.float32, device=self.device),
+        )
+
+        if y is not None:
+            event, time = check_array_survival(X, y)
+            ts += (
+                torch.tensor(event.copy(), dtype=torch.bool, device=self.device), 
+                torch.tensor(time.copy(), dtype=torch.float32, device=self.device),
+            )
+
+        return TensorDataset(*ts)
+
+
+    def _run_model(self, X, run_survival):
+        dataset = self._get_tensor_dataset(X, None)
+        loader = DataLoader(dataset, batch_size=self.batch_size, shuffle=False)
+        self.modules_.eval()
+        result_acc = {}
+        with torch.no_grad():
+            for batch in loader:
+                batch_results = self._run_model_batch(*batch, run_survival=run_survival)
+                for name, values in batch_results.items():
+                    result_acc.setdefault(name, []).append(values)
+            results = {
+                name: torch.concatenate(batches).cpu().numpy()
+                for name, batches in result_acc.items()
+            }
+        return results
+        
+    def _run_model_batch(self, xvae, xother, run_survival):
+        # FIXME option for not running decoder if we only want survival risk
+        r = self.modules_['VAE'](xvae)
+        if run_survival:
+            z = r['zrep'] # reparametrized latent space?
+            xsurv = torch.hstack([z, xother])
+            r['risk'] = self.modules_['survival'](xsurv)
+        return r
+
+    def _compute_losses(self, batch, run_survival):
+        xvae, xother, event, time = batch
+        r = self._run_model_batch(xvae, xother, run_survival=run_survival)
+        losses = {
+            'reconstruction': self.modules_['reconstruction_loss'](xvae, r['rec']),
+            'kl_divergence': -0.5 * torch.sum(
+                1 + r['logvar'] - r['mean'].pow(2) - r['logvar'].exp()),
+        }
+        losses['vae'] = (1.0 - self.kl_weight) * losses['reconstruction'] + self.kl_weight * losses['kl_divergence']
+        losses['survival'] = cox_ph_loss(r['risk'], time, event) if run_survival else torch.tensor(0.0)
+        losses['total'] = (1.0 - self.alpha) * losses['vae'] + self.alpha * losses['survival']
+        return losses, r
+
+    def _train(self, epoch, optimizer, train_loader, test_loader, train_survival):
+        self.modules_.train()
+        for batch in train_loader:
+            optimizer.zero_grad()
+            losses, _ = self._compute_losses(batch, run_survival=train_survival)
+            self.train_history_.add_batch_losses(epoch, 'train', losses)
+            losses['total'].backward()
+            optimizer.step()
+        
+        if test_loader is not None:
+            self.modules_.eval()
+            with torch.no_grad():
+                for batch in test_loader:
+                    losses, results = self._compute_losses(batch, run_survival=train_survival)
+                    self.train_history_.add_batch_losses(epoch, 'test', losses)
  
